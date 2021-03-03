a16939cd0893fdd8a60308cc32b1a88a238e1bbe
Parker, Bryant <parker_bryant1@baylor.edu>
2021-03-03 17:30:07 -0600

    finished

--- sitka5_deathvally.py
+++ sitka5_deathvally.py
@@ -27,16 +27,16 @@ fig = plt.figure()
 plt.plot(dates, highs, c="red")
 plt.plot(dates, lows, c="blue")
 
-fig.autofmt_xdate()
+# fig.autofmt_xdate()
 
-plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
+fill = plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
 
 plt.title("Daily high and low temperatures - 2018", fontsize=16)
 plt.xlabel("", fontsize=12)
 plt.ylabel("Temperature (F)", fontsize=12)
 plt.tick_params(axis="both", labelsize=12)
 
-plt.show()
+# plt.show()
 
 
 open_file = open("death_valley_2018_simple.csv", "r")
@@ -79,16 +79,24 @@ plt.xlabel("", fontsize=12)
 plt.ylabel("Temperature (F)", fontsize=12)
 plt.tick_params(axis="both", labelsize=12)
 
-plt.show()
+# plt.show()
 
 
 fig2, a = plt.subplots(2)
-a[0].plot(plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1))
+
+fig2.suptitle(
+    "Temperature comparison between SITKA AIRPORT, AK US and DEATH VALLY, CA US"
+)
+a[0].fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
 a[0].plot(dates, highs, c="red")
 a[0].plot(dates, lows, c="blue")
-
+a[0].title.set_text("SITKA AIRPORT, AK US")
+# a[0].title("Daily high and low temperatures - 2018", fontsize=16)
 
 a[1].plot(dates, highs1, c="red")
 a[1].plot(dates, lows1, c="blue")
 plt.fill_between(dates1, highs1, lows1, facecolor="blue", alpha=0.1)
+a[1].title.set_text(
+    "DEATH VALLY, CA US",
+)
 plt.show()
