270f6f55f582d4c08ef5aabef04ce9af198284b0
Parker, Bryant <parker_bryant1@baylor.edu>
2021-03-02 20:24:06 -0600

    sitka_2

---
+++ sitka_2.py
@@ -0,0 +1,35 @@
+import csv
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+open_file = open("sitka_weather_07-2018_simple.csv", "r")
+
+csv_file = csv.reader(open_file, delimiter=",")
+
+header_row = next(csv_file)
+
+for index, column_header in enumerate(header_row):
+    print("Index:", index, "Column Name:", column_header)
+
+highs = []
+dates = []
+
+for row in csv_file:
+    highs.append(int(row[5]))
+    converted_date = datetime.strptime(row[2], "%Y-%m-%d")
+    dates.append(converted_date)
+
+# print(highs)
+
+fig = plt.figure()
+
+plt.plot(dates, highs, c="red")
+
+fig.autofmt_xdate()
+
+plt.title("Daily high temperatures, July 2018", fontsize=16)
+plt.xlabel("", fontsize=12)
+plt.ylabel("Temperature (F)", fontsize=12)
+plt.tick_params(axis="both", labelsize=12)
+
+plt.show()
